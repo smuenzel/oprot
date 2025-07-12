6957089061dfbc9c6341b1a35bf84b228532a489
Stefan Muenzel <source@s.muenzel.net>
2025-07-12 07:16:52 +0200

    Better dsl

--- lib/dsl.ml
+++ lib/dsl.ml
@@ -11,6 +11,7 @@ module Annot = struct
        ; per_repeating_group : 'a4
        ; per_buffer : 'a5
        ; per_group : 'a6  
+       ; per_entry : 'a7
        >
 
   type 'a per_field = 'b constraint 'a = < per_field : 'b; .. >
@@ -19,40 +20,36 @@ module Annot = struct
   type 'a per_repeating_group = 'b constraint 'a = < per_repeating_group : 'b; .. >
   type 'a per_buffer = 'b constraint 'a = < per_buffer : 'b; .. >
   type 'a per_group = 'b constraint 'a = < per_group : 'b; .. >
+  type 'a per_entry = 'b constraint 'a = < per_entry : 'b; .. >
 end
 
 module rec Entry : sig
+  type ('a, 's) t' =
+    | Field : 'a Field.t -> ('a Annot.t, 'a Annot.per_field) t'
+    | Group : 'a Group.t -> ('a Annot.t, 'a Annot.per_group) t'
+    | Select : 'a Select.t -> ('a Annot.t, 'a Annot.per_select) t'
+    | Named_message : 'a Named_message.t -> ('a Annot.t, 'a Annot.per_named_message) t'
+    | Repeating_group : 'a Repeating_group.t -> ('a Annot.t, 'a Annot.per_repeating_group) t'
+    | Buffer : 'a Buffer.t -> ('a Annot.t, 'a Annot.per_buffer) t'
+
   type 'a t =
-    | Field of 'a Field.t
-    | Group of 'a Group.t
-    | Select of 'a Select.t
-    | Named_message of 'a Named_message.t
-    | Repeating_group of 'a Repeating_group.t
-    | Buffer of 'a Buffer.t
-    constraint 'a = 'a0 Annot.t
+      E : { t : ('a, 's) t'
+          ; per_entry : 'a Annot.per_entry
+          ; specific : 's
+          } -> 'a t
 end = Entry
 and Field : sig
-  type t' =
+  type _ t =
     | F : { constraint_ : 't Constraint.t option
           ; typ : 't Typ.t
           ; name : Field_name.t option
-          } -> t'
-
-  type 'a t =
-    { t : t'
-    ; annot : 'a Annot.per_field
-    }
+          } -> _ t
 end = Field
 and Group : sig
-  type 'a t' =
+  type 'a t =
     { optional : bool
     ; name : Field_name.t option
     ; entries : 'a Entry.t list
-    }
-
-  type 'a t =
-    { t : 'a t'
-    ; annot : 'a Annot.per_group
     }
 end = Group
 and Select : sig
@@ -63,50 +60,30 @@ and Select : sig
         }
         -> 'a selector
 
-  type 'a t' =
+  type 'a t =
     { default : 'a Entry.t option
     ; name : Field_name.t
     ; selector : 'a selector
     }
-
-  type 'a t =
-    { t : 'a t'
-    ; annot : 'a Annot.per_select
-    }
 end = Select
 and Named_message : sig
-  type 'a t' =
+  type 'a t =
     { scope : Scope_name.t option
     ; name : Message_name.t
     ; entry : 'a Entry.t
     }
-
-  type 'a t =
-    { t : 'a t'
-    ; annot : 'a Annot.per_named_message
-    }
 end = Named_message
 and Repeating_group : sig
-  type 'a t' =
+  type 'a t =
     { count : int Constraint.Pre.t option
     ; name : Field_name.t
     ; entries : 'a Entry.t list
     }
-
-  type 'a t =
-    { t : 'a t'
-    ; annot : 'a Annot.per_repeating_group
-    }
 end = Repeating_group
 and Buffer : sig
-  type 'a t' =
+  type 'a t =
     { length : int Constraint.Pre.t option
     ; name : Field_name.t
-    }
-
-  type 'a t =
-    { t : 'a t'
-    ; annot : 'a Annot.per_buffer
     }
 end = Buffer
 
@@ -118,31 +95,51 @@ module D = struct
       | Some constraint_ -> Some (Constraint.make constraint_ typ)
       | None -> None
     in
-    Entry.Field
-      { Field.t = F { constraint_; typ; name = Some name}; annot = () }
+    Entry.E
+      { t = Field (F { constraint_; typ; name = Some name})
+      ; per_entry = ()
+      ; specific = ()
+      }
 
   let field_reserved typ =
-    Entry.Field
-      { Field.t = F { constraint_ = None; typ; name = None}; annot = () }
+    Entry.E
+      { t = Field (F { constraint_ = None; typ; name = None})
+      ; per_entry = ()
+      ; specific = ()
+      }
 
   let group ?(optional = false) ?name entries =
-    Entry.Group
-      { Group.t = { optional; name; entries}; annot = () }
+    Entry.E
+      { t = Group { optional; name; entries}
+      ; per_entry = ()
+      ; specific = ()
+      }
 
   let select ?default name typ entries =
-    Entry.Select
-      { Select.t = { default; name; selector = S { typ; cases = entries } }; annot = () }
+    Entry.E
+      { t = Select { default; name; selector = S { typ; cases = entries } }
+      ; per_entry = ()
+      ; specific = ()
+      }
 
   let message_name ?scope name entry =
-    Entry.Named_message
-      { Named_message.t = { scope; name; entry }; annot = () }
+    Entry.E
+      { t = Named_message { scope; name; entry }
+      ; per_entry = ()
+      ; specific = ()
+      }
 
   let repeating_group ?count name entries =
-    Entry.Repeating_group
-      { Repeating_group.t = { count; name; entries }; annot = () }
+    Entry.E
+      { t = Repeating_group { count; name; entries }
+      ; per_entry = ()
+      ; specific = ()
+      }
 
   let buffer ?length name =
-    Entry.Buffer
-      { Buffer.t = { length; name }; annot = () }
-
+    Entry.E
+      { t = Buffer { length; name }
+      ; per_entry = ()
+      ; specific = ()
+      }
 end
